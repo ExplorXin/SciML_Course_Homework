5c531c730e46f09a05e1c0d636f85e128fe3a198
xin <longxin.c@outlook.com>
2026-03-24 13:35:49 +0800

    第一次提交

---
+++ hom1/net.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FNN(nn.Module):
+    def __init__(self, layers, actn = 'relu'):
+        super().__init__()
+        L = len(layers)
+
+
+        self.linear = nn.ModuleList([nn.Linear(layers[l-1], layers[l]) for l in range(1, L)])
+
+        # 激活函数
+        if actn == 'relu':
+            actn = nn.ReLU()
+        elif actn == 'tanh':
+            actn = nn.Tanh()
+        elif actn == 'sigmoid':
+            actn = nn.Sigmoid()
+        self.activation = actn
+
+    def forward(self, inputs):
+        x = inputs
+        for linear in self.linear[:-1]:
+            x = self.activation(linear(x))
+        x = self.linear[-1](x)
+        return x
+
